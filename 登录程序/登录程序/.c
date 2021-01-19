157e50fde8eff8ba3f68233ba9c75ffc99fae101
教父 <1819443212@qq.com>
2021-01-19 19:42:36 +0800

    实现登录程序

---
+++ 登录程序/登录程序/.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	int i = 0;
+	char password[20] = { 0 };
+	for (i = 0; i < 3; i++)
+	{
+		printf("ÇëÊäÈëÃÜÂë£º");
+		scanf("%s", password);
+		if (strcmp(password, "123456") == 0)
+		{
+			printf("µÇÂ½³É¹¦\n");
+			break;
+		}
+		else
+		{
+			printf("ÃÜÂë´íÎó\n");
+		}
+	}
+	if (i == 3)
+		printf("Èý´ÎÃÜÂë¾ù´íÎó£¬ÇëÍË³ö³ÌÐò\n");
+	return 0;
+}
