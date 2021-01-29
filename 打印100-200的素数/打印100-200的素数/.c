e671fc71cfddb30c48badef8f7f71fb2a12808b5
教父 <1819443212@qq.com>
2021-01-29 22:07:18 +0800

    打印100-200的素数

---
+++ 打印100-200的素数/打印100-200的素数/.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+	int i = 0;
+	int count = 0;
+	for (i = 100; i <= 200; i++)
+	{
+		int j = 0;
+		for (j = 2 ; j <=sqrt(i); j++)
+		{
+			if (i%j == 0)
+			{
+				break;
+			}
+		}
+		if (j > sqrt(i))
+		{
+			count++;
+			printf("%d ",i);
+		}
+	}
+	printf("\ncount=%d\n", count);
+	return 0;
+}
