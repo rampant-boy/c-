1eee2c656b4f3fb21160d95fa9e2ad656cac679e
教父 <1819443212@qq.com>
2021-02-06 20:44:02 +0800

    将数字分别打印

---
+++ 将数字分别打印/将数字分别打印/.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void print(int n)
+{
+	if (n >9)
+	{
+		print(n / 10);
+	}
+	printf("%d ", n % 10);
+}
+
+int main()
+{
+	unsigned int num = 0;
+	scanf("%d", &num);
+	print(num);
+	return 0;
+}
