355bf0a0d6ea0f828214c3db8d24471cf794c721
教父 <1819443212@qq.com>
2021-02-07 23:15:01 +0800

    交换两个变量（不创建第三个变量）

---
+++ 交换两个变量（不创建第三个变量）/交换两个变量（不创建第三个变量）/.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int a = 3;
+	int b = 5;
+	printf("before:a=%d,b=%d\n", a, b);
+	a = a^b;
+	b = a^b;
+	a = a^b;
+	printf("after: a=%d,b=%d\n", a, b);
+	return 0;
+}
