7b2d4e1d5aee4eb9d6b139243ce654bd860c2cf8
教父 <1819443212@qq.com>
2021-03-22 23:31:28 +0800

    用函数实现strstr函数

---
+++ 用函数实现strstr函数/用函数实现strstr函数/test.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<assert.h>
+
+char* my_strstr(const char* p1, const char* p2)
+{
+	assert(p1 != NULL);
+	assert(p2 != NULL);
+	char* s1 = p1;
+	char* s2 = p2;
+	char* cur = (char*)p1;//因为p1受保护
+	if (*p2 == '\0')
+	{
+		return (char*)p1;
+	}
+	while (*cur)
+	{
+		s1 = cur;
+		s2 = (char*)p2;
+		while ((*s1 != '\0') && (*s2 != '\0') && (*s1 == *s2))
+		{
+			s1++;
+			s2++;
+		}
+		if (*s2 == '\0')
+		{
+			return cur;//找到子串
+		}
+		cur++;
+	}
+	return NULL;//找不到子串
+}
+
+int main()
+{
+	char* p1 = "abbbcdef";
+	char* p2 = "bbc";
+	char* ret = my_strstr(p1, p2);
+	if (ret == NULL)
+	{
+		printf("子串不存在\n");
+	}
+	else
+	{
+		printf("%s\n",ret);
+	}
+	return 0;
+}
